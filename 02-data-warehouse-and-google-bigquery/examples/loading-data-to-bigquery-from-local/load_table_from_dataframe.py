c1c1752c8cdb23ba2a70e5054c37c5f7f33a744b
wutcharin munjai <wutcharinmunjai@wutcharins-MacBook-Pro-2.local>
2023-04-23 17:11:28 +0700

    done 2nd day

--- 02-data-warehouse-and-google-bigquery/examples/loading-data-to-bigquery-from-local/load_table_from_dataframe.py
+++ 02-data-warehouse-and-google-bigquery/examples/loading-data-to-bigquery-from-local/load_table_from_dataframe.py
@@ -12,38 +12,82 @@ from google.oauth2 import service_account
 keyfile = os.environ.get("KEYFILE_PATH")
 service_account_info = json.load(open(keyfile))
 credentials = service_account.Credentials.from_service_account_info(service_account_info)
-project_id = "dataengineercafe"
+project_id = "data-en-bootcamp"
 client = bigquery.Client(
     project=project_id,
     credentials=credentials,
 )
 
-job_config = bigquery.LoadJobConfig(
-    write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE,
-    schema=[
-        bigquery.SchemaField("user_id", bigquery.SqlTypeNames.STRING),
-        bigquery.SchemaField("first_name", bigquery.SqlTypeNames.STRING),
-        bigquery.SchemaField("last_name", bigquery.SqlTypeNames.STRING),
-        bigquery.SchemaField("email", bigquery.SqlTypeNames.STRING),
-        bigquery.SchemaField("phone_number", bigquery.SqlTypeNames.STRING),
-        bigquery.SchemaField("created_at", bigquery.SqlTypeNames.TIMESTAMP),
-        bigquery.SchemaField("updated_at", bigquery.SqlTypeNames.TIMESTAMP),
-        bigquery.SchemaField("address_id", bigquery.SqlTypeNames.STRING),
-    ],
-    time_partitioning=bigquery.TimePartitioning(
-        type_=bigquery.TimePartitioningType.DAY,
-        field="created_at",
-    ),
-    clustering_fields=["first_name", "last_name"],
-)
+dataset_name = "users"
+is_partition_require = True
 
-file_path = "users.csv"
-df = pd.read_csv(file_path, parse_dates=["created_at", "updated_at"])
-df.info()
+datasetList = [
+    {
+        "dataset_name": "addresses",
+        "is_partition_require": False,
+        "parse_dates": None
+    },
+    {
+        "dataset_name": "events",
+        "is_partition_require": True,
+        "parse_dates": ["created_at"]
+    },
+    {
+        "dataset_name": "order_items",
+        "is_partition_require": False,
+        "parse_dates": None
+    },
+    {
+        "dataset_name": "orders",
+        "is_partition_require": True,
+        "parse_dates": ["created_at", "estimated_delivery_at", "delivered_at"]
+    },
+    {
+        "dataset_name": "products",
+        "is_partition_require": False,
+        "parse_dates": None
+    },
+    {
+        "dataset_name": "promos",
+        "is_partition_require": False,
+        "parse_dates": None
+    },
+    {
+        "dataset_name": "users",
+        "is_partition_require": True,
+        "parse_dates": ["created_at", "updated_at"]
+    }
+]
 
-table_id = f"{project_id}.dbt_zkan.users_df"
-job = client.load_table_from_dataframe(df, table_id, job_config=job_config)
-job.result()
+for dataset in datasetList:
+    print(f"- start upload for dataset name : {dataset['dataset_name']}")
+    if (dataset['is_partition_require'] == True):
+        job_config = bigquery.LoadJobConfig(
+            write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE,
+            autodetect=True,
+            time_partitioning=bigquery.TimePartitioning(
+                type_=bigquery.TimePartitioningType.DAY,
+                field="created_at"
+            )
+        )
+        file_path = f"{dataset['dataset_name']}.csv"
+        df = pd.read_csv(file_path, parse_dates=dataset['parse_dates'])
+        # df = pd.read_csv(file_path)
+        df.info()
+    else:
+        job_config = bigquery.LoadJobConfig(
+            write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE,
+            autodetect=True
+        )
+        file_path = f"{dataset['dataset_name']}.csv"
+        # df = pd.read_csv(file_path, parse_dates=["created_at"])
+        df = pd.read_csv(file_path)
+        df.info()
 
-table = client.get_table(table_id)
-print(f"Loaded {table.num_rows} rows and {len(table.schema)} columns to {table_id}")
+    table_id = f"{project_id}.deb_bootcamp.{dataset['dataset_name']}"
+    job = client.load_table_from_dataframe(df, table_id, job_config=job_config)
+    job.result()
+
+    table = client.get_table(table_id)
+    print(f"- end upload for dataset name : {dataset['dataset_name']}")
+    print(f"Loaded {table.num_rows} rows and {len(table.schema)} columns to {table_id}")
